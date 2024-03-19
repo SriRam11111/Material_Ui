f09d740bd60cce04a89d6deee46edd6455026a00
SriRam11111 <vijayaram4501@gmail.com>
2024-03-20 00:36:37 +0530

    initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+import "./App.css";
+import Header from "./components/header";
+
+function App() {
+  return (
+          <Container maxWidth='md'>
+          <Header />
+          </Container>
+   
+  );
+}
+
+export default App;
